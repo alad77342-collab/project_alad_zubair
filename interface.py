a949aac0e25f3ed3c1fa0fbaec7886612286f4a0
alad77342-collab <alad77342@gmail.com>
2026-05-03 16:17:44 +0530

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,7 @@
+from model import CNN_Emoji as TheModel
+from train import Train as the_trainer
+from predict import
+from dataset import Emojis as TheDataset
+from dataset import trainloader as the_dataloader
+from config import batch_size as the_batch_size
+from config import epochs as total_epochs
